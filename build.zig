8151db0ad9f5af33fe73f539c240b67c805ba9ca
pfg <pfg@pfg.pw>
2023-08-04 11:25:00 -0700

    compile some msdfgen files; next: freetype

--- build.zig
+++ build.zig
@@ -1,6 +1,40 @@
 const std = @import("std");
 const mach = @import("libs/mach/build.zig");
 
+fn linkMsdfGen(b: *std.Build, app: *std.build.Step.Compile) !void {
+    _ = b;
+    app.linkSystemLibrary("c++");
+    app.addIncludePath("libs/msdfgen/");
+    for(&[_][]const u8{
+        "libs/msdfgen/core/contour-combiners.cpp",
+        "libs/msdfgen/core/Contour.cpp",
+        "libs/msdfgen/core/edge-coloring.cpp",
+        "libs/msdfgen/core/edge-segments.cpp",
+        "libs/msdfgen/core/edge-selectors.cpp",
+        "libs/msdfgen/core/EdgeHolder.cpp",
+        "libs/msdfgen/core/equation-solver.cpp",
+        "libs/msdfgen/core/msdf-error-correction.cpp",
+        "libs/msdfgen/core/MSDFErrorCorrection.cpp",
+        "libs/msdfgen/core/msdfgen.cpp",
+        "libs/msdfgen/core/Projection.cpp",
+        "libs/msdfgen/core/rasterization.cpp",
+        "libs/msdfgen/core/render-sdf.cpp",
+        "libs/msdfgen/core/save-bmp.cpp",
+        "libs/msdfgen/core/save-tiff.cpp",
+        "libs/msdfgen/core/Scanline.cpp",
+        "libs/msdfgen/core/sdf-error-estimation.cpp",
+        "libs/msdfgen/core/shape-description.cpp",
+        "libs/msdfgen/core/Shape.cpp",
+        "libs/msdfgen/core/SignedDistance.cpp",
+        "libs/msdfgen/core/Vector2.cpp",
+
+        // "libs/msdfgen/ext/import-font.cpp",
+        // "libs/msdfgen/ext/resolve-shape-geometry.cpp",
+    }) |cpp_file| {
+        app.addCSourceFile(cpp_file, &.{});
+    }
+}
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -22,6 +56,7 @@ pub fn build(b: *std.Build) !void {
         },
         .optimize = optimize,
     });
+    try linkMsdfGen(b, app.compile);
     try app.link(.{});
     app.compile.step.dependOn(&codegen_step.step);
 
