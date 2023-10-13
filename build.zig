17dca971a4b348ead403456888a7b8f5db7c114b
pfg <pfg@pfg.pw>
2023-10-12 22:35:54 -0400

    building again but it crashes

--- build.zig
+++ build.zig
@@ -91,6 +91,7 @@ pub fn build(b: *std.Build) !void {
         },
         .optimize = optimize,
     });
+    @import("mach_freetype").linkFreetype(mach_freetype_dep.builder, app.compile);
     try linkMsdfGen(b, app.compile);
     app.compile.step.dependOn(&codegen_step.step);
 
