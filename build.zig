91501377a75e6d6ca65ddbb4c169985a55fd78b3
pfg <pfg@pfg.pw>
2023-10-16 13:25:13 -0400

    not sure how to use zignetwork

--- build.zig
+++ build.zig
@@ -83,12 +83,15 @@ pub fn build(b: *std.Build) !void {
                 .name = "mach-sysaudio",
                 .module = mach_sysaudio_dep.module("mach-sysaudio"),
             },
+            std.Build.ModuleDependency{
+                .name = "network",
+                .module = b.dependency("network", .{}).module("network"),
+            },
         },
         .optimize = optimize,
     });
     @import("mach_freetype").linkFreetype(mach_freetype_dep.builder, app.compile);
     @import("mach_sysaudio").link(mach_sysaudio_dep.builder, app.compile);
-    app.compile.addModule("network", b.dependency("network", .{}).module("network"));
     try linkMsdfGen(b, app.compile);
 
     if (b.args) |args| app.run.addArgs(args);
